7a1a8bd55394abfe7ea62d6ca128eee15041aed9
Joakim Knudsen <joakim.knudsen@dfind.no>
2024-06-30 15:26:45 +0200

    Adjusted deploy script

--- test.pl
+++ test.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+my $full = $ARGV[0];
+
 # Backup original package.json
 print "Making copy of package.json\n";
 system("cp package.json package_local.json");
@@ -24,9 +26,22 @@ die "npm build failed: $?" if $? != 0;
 
 # Transfer build artifacts to remote server
 print "Copying build artifacts to remote server\n";
-my $scp_command = "scp -r -i ~/.ssh/gknu build/* gknudsen\@login.domeneshop.no:www/startskjerm";
-system($scp_command);
-die "Failed to execute $scp_command: $?" if $? != 0;
+
+if ($full) {
+    my $scp_command = "scp -r -i ~/.ssh/gknu build/* gknudsen\@login.domeneshop.no:www/startskjerm";
+    system($scp_command);
+    die "Failed to execute $scp_command: $?" if $? != 0;
+
+} else {
+
+    my $scp_command = "scp -r -i ~/.ssh/gknu build/static/* gknudsen\@login.domeneshop.no:www/startskjerm/static";
+    system($scp_command);
+    die "Failed to execute $scp_command: $?" if $? != 0;
+
+    my $scp_command2 = "scp -r -i ~/.ssh/gknu build/index.html gknudsen\@login.domeneshop.no:www/startskjerm";
+    system($scp_command2);
+    die "Failed to execute $scp_command2: $?" if $? != 0;
+}
 
 # Restore package.json for local server
 print "Restore package.json for local server\n";
