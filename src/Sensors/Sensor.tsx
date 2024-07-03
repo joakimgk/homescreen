7e064c5cd7d792cceefdd261e9d54268b47c8f78
Joakim Knudsen <joakim.knudsen@dfind.no>
2024-07-04 00:28:56 +0200

    Sensor data

--- src/Sensors/Sensor.tsx
+++ src/Sensors/Sensor.tsx
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components"
 import { useData } from "../services/sensorService";
-import { Client, Activity } from "../../types/supabaseTypes";
+import { Client, Activity, Event } from "../../types/supabaseTypes";
 import dayjs from "dayjs";
 import { Link } from "wouter";
 
@@ -14,11 +14,46 @@ const Wrapper = styled.div<{ inactive: string, status: string }>`
     background-color: ${props => props.status};
 `;
 
+const statusColor = (status?: SensorStatus): string => {
+    switch (status) {
+        case SensorEnum.Red:
+            return "red";
+        case SensorEnum.Green:
+            return "green";
+        case SensorEnum.Blue:
+            return "blue";
+        default:
+            return "grey";
+    }
+}
+
+export enum SensorEnum {
+    Green = "0",
+    Red = "1",
+    Blue = "2"
+}
+export type SensorStatus = SensorEnum;
+
+const LOCK_SENSOR = 'button1', DOOR_SENSOR = 'button2';
+
+export const getStatus = (event: Event | Activity): SensorStatus => {
+    if (event.button_id === DOOR_SENSOR) {
+        if (event.button_state === "on") {
+            return SensorEnum.Red;
+        }
+        return SensorEnum.Blue;
+    }
+    if (event.button_state === "on") {
+        return SensorEnum.Green;
+    }
+    return SensorEnum.Red;
+}
+
 // alarm om dør er åpen og det er regn!
 // åpen mellom 22-09, eller i arbeidstid
 export const Sensor = ({ data }: { data: Client }) => {
 
-    const { data: lastActivity } = useData<Activity[]>('v_event_last_activity', 'client_id=eq.' + data.id);
+    const { data: lastActivity } = useData<Activity[]>('v_event_last_activity', 'client_id=eq.' + data.id + '&order=created_at.desc');
 
     const lockSensor = lastActivity?.find(a => a.button_id === 'button1');
     const doorSensor = lastActivity?.find(a => a.button_id === 'button2');
@@ -26,17 +61,7 @@ export const Sensor = ({ data }: { data: Client }) => {
     const isInactive = (time?: string | null) =>
         dayjs(time, 'YYYY-MM-DDTHH:MM:SS', 'no').isBefore(dayjs().add(-14, 'days'));
 
-    const getStatus = () => {
-        let color = 'green';
-        if (!lockSensor || !doorSensor) return 'grey';
-        if (lockSensor?.button_state !== 'on') {
-            color = 'red';
-            if (doorSensor?.button_state !== 'on') {
-                color = 'blue';
-            }
-        }
-        return color;
-    }
+    const status = lastActivity ? getStatus(lastActivity[0]) : undefined;
 
     const inactive = isInactive(lockSensor?.last_active) || isInactive(doorSensor?.last_active);
 
@@ -49,8 +74,9 @@ export const Sensor = ({ data }: { data: Client }) => {
     return (
         <>
             <Link href={link}>
-                <Wrapper inactive={inactive.toString()} status={getStatus()}>
+                <Wrapper inactive={inactive.toString()} status={statusColor(status)}>
                     {/* {data.name} */}
+                    {/* {lastActivity && lastActivity[0].button_id === 'button1' ? 'lock' : 'door'} */}
                 </Wrapper >
             </Link>
         </>
